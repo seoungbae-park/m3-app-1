ca54793723fbfc19e312f8205d8983bd1151ea03
seoungbae.park@nuvilab.com <seoungbae.park@nuvilab.com>
2024-05-21 08:19:36 +0000

    Created Screenshot 2024-05-21 at 5.14.45 PM.png and LICENSE.txt; Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -8,3 +8,12 @@ class Form1(Form1Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def image_1_show(self, **event_args):
+    """This method is called when the Image is shown on the screen"""
+    pass
+
+  def text_box_1_pressed_enter(self, **event_args):
+    """This method is called when the user presses Enter in this text box"""
+    # access i
+    pass
